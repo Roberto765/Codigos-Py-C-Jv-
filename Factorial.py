d5c49c74feae57f837e2f15b6ce3733a59a4cbe8
Roberto765 <158394433+Roberto765@users.noreply.github.com>
2024-02-13 20:30:43 -0600

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -15,18 +15,7 @@ def main():
 if __name__ == "__main__":
     main()
 
-    #No hay uso de clases ni objetos
-    #Se adhiere al paradigma de la programación estructurada.
 
-    # Este paradigma se centra en dividir el programa en 
-    #funciones lógicas y secuenciales, donde cada función realiza una tarea específica.
-
-    #Se considera debilmente equipado debido a su simplicidad
-
-    #Es considerado un multiparadigma debido a qe puedes emplear varios
-    #Estilos de progrmacion
-
-    #Utiliza un tipo de datos integrados
 
 
 
